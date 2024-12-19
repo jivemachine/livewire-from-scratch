d9b6cd176b56dfe990e1ed9e63f86dbb4841bdd7
jivemachine <the.jeremy.cobb@gmail.com>
2024-12-19 16:22:42 -0600

    removed formatter

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,13 +1,22 @@
 <div>
-    <form wire:submit="changeName(document.querySelector('#newName').value)">
+    <form
+        wire:submit="changeName(document.querySelector('#newName').value)"
+    >
         <div>
             Hello, {{ $name }}!
         </div>
         <div class="mt-2">
-            <input id="newName" type="text" class="block w-full p-4 text-white bg-gray-700 border rounded-md">
+            <input
+                id="newName"
+                type="text"
+                class="block w-full p-4 text-white bg-gray-700 border rounded-md"
+            >
         </div>
         <div class="mt-2">
-            <button type="submit" class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md">
+            <button
+                type="submit"
+                class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md"
+            >
                 Greet
             </button>
         </div>
