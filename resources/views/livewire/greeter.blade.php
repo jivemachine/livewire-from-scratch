9efc8c6f567e087e2bf829f12e6a2fdeee2bf8b4
jivemachine <the.jeremy.cobb@gmail.com>
2024-12-19 16:52:27 -0600

    Episode 4: - data validation with livewire

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,6 +1,6 @@
 <div>
     <form
-        wire:submit="changeName()"
+        wire:submit="changeGreeting()"
     >
         <div class="mt-2">
             <select
@@ -19,6 +19,11 @@
                 wire:model="name"
             >
         </div>
+        <div>
+            @error('name')
+            {{$message}}
+            @enderror
+        </div>
         <div class="mt-2">
             <button
                 type="submit"
@@ -30,7 +35,7 @@
     </form>
     @if ($name !== '')
         <div class="mt-5">
-            {{$greeting}}, {{ $name }}!
+            {{$greetingMessage}}
         </div>
     @endif
 </div>
