617106110690baec6b4a7811a1a7b8ca4be3d0d8
Jeremy <the.jeremy.cobb@gmail.com>
2025-01-03 16:57:46 -0600

    Episode 10

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -926,10 +926,8 @@
             </div>
         </div>
     </div>
-    <script>
-        document.addEventListener('search:clear-results', function(e) {
-            console.log("cleared results");
-        });
+    <script data-navigate-once>
+        console.log("Page loaded.");
     </script>
 </body>
 
