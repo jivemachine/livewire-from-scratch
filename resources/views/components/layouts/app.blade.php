8c2550a5124d73df72cdb813f108970ec578af65
Jeremy <the.jeremy.cobb@gmail.com>
2025-01-03 16:20:45 -0600

    Episode 7

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -918,7 +919,7 @@
                 </header>
 
                 <main class="mt-6">
-
+                    {{ $slot }}
                 </main>
             </div>
         </div>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,26 +1,20 @@
 <div>
     <form>
         <div class="mt-2">
-            <input
-                type="text"
-                class="w-9/12 p-4 text-white bg-gray-700 border rounded-md"
-                wire:model.live.debounce="searchText"
-                placeholder="Search..."
-            >
+            <input type="text" class="w-9/12 p-4 text-white bg-gray-700 border rounded-md"
+                wire:model.live.debounce="searchText" placeholder="Search...">
 
             <button class="p-4 font-medium text-white bg-indigo-600 rounded-md disabled:bg-indigo-400"
-                wire:click.prevent="clear()"
-                {{ empty($searchText) ? 'disabled' : '' }}
-            >
+                wire:click.prevent="clear()" {{ empty($searchText) ? 'disabled' : '' }}>
                 Clear
             </button>
         </div>
     </form>
     <div class="mt-4">
-        @foreach($results as $result)
-        <div class="pt-2">
-            {{$result->title}}
-        </div>
+        @foreach ($results as $result)
+            <div class="pt-2">
+                <a href="/articles/{{ $result->id }}">{{ $result->title }}</a>
+            </div>
         @endforeach
     </div>
 </div>
