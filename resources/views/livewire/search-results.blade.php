c060fe65501b6dea16225d8c62bc781f3a1cd337
Jeremy <the.jeremy.cobb@gmail.com>
2025-01-03 16:51:35 -0600

    Episode 9

--- resources/views/livewire/search-results.blade.php
+++ resources/views/livewire/search-results.blade.php
@@ -1,5 +1,8 @@
 <div class="{{ $show ? 'block' : 'hidden' }}">
     <div class="absolute p-4 mt-4 bg-gray-700 border border-indigo-600 rounded-md">
+        {{-- <div class="absolute top-0 right-0 pt-1 pr-3">
+            <button type="button" wire:click="dispatch('search:clear-results')">x</button>
+        </div> --}}
         @if (count($results) == 0)
             <p>No results found.</p>
         @endif
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,13 +1,9 @@
 <div>
     <form>
         <div class="mt-2">
-            <input type="text" class="w-9/12 p-4 text-white bg-gray-700 border rounded-md"
+            <input type="text" class="w-full p-4 text-white bg-gray-700 border rounded-md"
                 wire:model.live.debounce="searchText" placeholder="{{ $placeholder }}">
 
-            <button class="p-4 font-medium text-white bg-indigo-600 rounded-md disabled:bg-indigo-400"
-                wire:click.prevent="clear()" {{ empty($searchText) ? 'disabled' : '' }}>
-                Clear
-            </button>
         </div>
     </form>
     <livewire:search-results :results="$results" :show="!empty($searchText)" />
